ef193aaa96c06e2b21d28689111721f0b8902d24
Amalendu Pandey <amalendupandey56@gmail.com>
2024-08-06 06:00:29 +0530

    basic folder structure is completed and working on themeData

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:tuitorial/utils/theme/custom_themes/text_theme.dart';
+
+class AppTheme {
+  AppTheme._();
+  static ThemeData lightTheme = ThemeData(
+      useMaterial3: true,
+      fontFamily: 'Poppins',
+      brightness: Brightness.light,
+      primaryColor: Colors.blue,
+      scaffoldBackgroundColor: Colors.white,
+      textTheme: TTextTheme.lightTextTheme,
+      elevatedButtonTheme: ElevatedButtonThemeData());
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: TTextTheme.darkTextTheme,
+  );
+}
