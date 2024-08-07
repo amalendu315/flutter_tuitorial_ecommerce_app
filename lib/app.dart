9518d62bb8028d36e215942d772c6b32fbde06a4
Amalendu Pandey <amalendupandey56@gmail.com>
2024-08-08 00:12:37 +0530

    Splash Screen YAML added and splash screen used too

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:tuitorial/utils/theme/theme.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tuitorial/utils/theme/theme.dart';
+import 'package:tuitorial/app.dart';
 
 void main() {
   //TODO:-Add Widget Binding
@@ -8,19 +8,5 @@ void main() {
   //TODO:-Initialize Firebase
   //TODO:-Initialize Authentication
 
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-    );
-  }
+  runApp(const App());
 }
